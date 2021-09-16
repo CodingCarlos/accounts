73fa099e53291282b31709dfdb8c0bc60c692c8d
Ivo <ivo.vilches@gmail.com>
2021-09-16 18:37:33 +0200

    Create task.vue

---
+++ layouts/task.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar
+      fixed
+      flat
+      app
+    >
+      <nuxt-link to="/">
+        <v-toolbar-title v-text="title" />
+      </nuxt-link>
+      <v-spacer />
+      <v-btn
+        icon
+      >
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <Nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Community Tools'
+    }
+  }
+}
+</script>

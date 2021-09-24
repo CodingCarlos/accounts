2ec5f4829a4bc4931d6740fba4176e982af07bac
Ivo <ivo.vilches@gmail.com>
2021-09-24 14:53:32 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,20 +4,55 @@
       fixed
       flat
       app
+      class="white"
     >
-      <a href="https://communitytools.co">
+
+      <nuxt-link to="/">
         <v-toolbar-title v-text="title" />
-      </a>
-      <v-spacer />
-      <v-btn
-        icon
+      </nuxt-link>
+
+      <v-row
+        horizontal
+        class="elevation-0"
       >
-        <v-icon>mdi-account</v-icon>
-      </v-btn>
+        <v-btn
+          text
+        >
+          <span>Product</span>
+        </v-btn>
+
+        <v-btn
+          text
+        >
+          <span>Pricing</span>
+        </v-btn>
+
+        <v-btn
+          text
+        >
+          <span>About</span>
+        </v-btn>
+
+        <v-btn
+          text
+        >
+          <span>Help</span>
+        </v-btn>
+      </v-row>
+
+      <nuxt-link to="/create">
+        <CTButton
+        >
+          get started
+        </CTButton>
+      </nuxt-link>
+
     </v-app-bar>
+
     <v-main class="pb-0">
       <Nuxt />
     </v-main>
+
   </v-app>
 </template>
 

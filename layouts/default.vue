f43a019ef0cac6148d2b4015b0b05bf622003dbf
Ivo <ivo.vilches@gmail.com>
2021-10-04 18:40:34 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,6 @@
       fixed
       class="white justify-space-between px-lg-6"
     >
-
       <nuxt-link to="/">
         <!-- <v-toolbar-title v-text="title" /> -->
         <v-img
@@ -15,10 +14,10 @@
           src="https://communitytools.co/img/logo.svg"
           class="mr-8"
           contain
-        ></v-img>
+        />
       </nuxt-link>
 
-      <v-spacer class="d-lg-none"></v-spacer>
+      <v-spacer class="d-lg-none" />
 
       <v-row
         horizontal
@@ -83,105 +82,75 @@
         to="/login"
         class="hidden-md-and-down"
       >
-        <CTButton
-        >
+        <CTButton>
           Log in
         </CTButton>
       </nuxt-link>
 
       <nuxt-link
         to="/create"
-        class="hidden-lg-and-up">
+        class="hidden-lg-and-up"
+      >
         <CTButton
           contained
         >
           Get started
         </CTButton>
       </nuxt-link>
-
     </v-app-bar>
 
     <v-main class="pb-0">
-      <!--  -->
-      <!-- <v-sheet
-        height="200px"
-        color="white"
-        elevation="1"
-        class="fixed cookies"
-      >
-        <v-img
-          :aspect-ratio="16/9"
-          :width="width"
-          src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
-          class="mb-4"
-          style="margin-top: -100px;"
-        ></v-img>
-        <div>
-          <p class="text-h5">Your data</p>
-          <p>Your data</p>
-        </div>
-        <div>
-          <CTButton></CTButton>
-          <CTButton></CTButton>
-          <CTButton></CTButton>
-        </div>
-      </v-sheet> -->
-      <!-- / -->
-
       <!-- Cookies -->
-      <template>
-        <v-row justify="center">
-          <v-dialog
-            v-model="dialog"
-            persistent
-            max-width="400"
+      <v-row justify="center">
+        <v-dialog
+          v-model="dialog"
+          persistent
+          max-width="400"
+        >
+          <v-card
+            class="pt-1 pb-4 px-4"
           >
-            <v-card
-              class="pt-1 pb-4 px-4"
+            <v-img
+              :aspect-ratio="16/9"
+              src="http://pngimg.com/uploads/cookie/cookie_PNG13696.png"
+              class="mt-3 mx-6 mb-0"
+              contain
+            />
+            <v-card-title class="text-h4 word-break-word">
+              Before activating your community...
+            </v-card-title>
+            <v-card-text>This site uses other services (like the chat) to communicate with you and other stuff. You can accept that we share some of your data with these services, configure which data we share and how or reject our cookies.</v-card-text>
+            <v-card-actions
+              class="flex-column pb-4"
             >
-              <v-img
-                :aspect-ratio="16/9"
-                :width="width"
-                src="http://pngimg.com/uploads/cookie/cookie_PNG13696.png"
-                class="mt-3 mx-6 mb-0"
-                contain
-              ></v-img>
-              <v-card-title class="text-h4 word-break-word">
-                Before activating your community...
-              </v-card-title>
-              <v-card-text>This site uses other services (like the chat) to communicate with you and other stuff. You can accept that we share some of your data with these services, configure which data we share and how or reject our cookies.</v-card-text>
-              <v-card-actions
-                class="flex-column pb-4"
+              <CTButton
+                color="primary"
+                block
+                contained
+                class="mb-2 elevation-0"
+                @click="dialog = false"
               >
-                <CTButton
-                  color="primary"
-                  block
-                  contained
-                  class="mb-2 elevation-0"
-                  @click="dialog = false"
-                >
-                  Accept all
-                </CTButton>
-                <CTButton
-                  color="primary"
-                  block
-                  contained
-                  class="mb-2 ml-0 elevation-0"
-                >
-                  Reject all
-                </CTButton>
-                <CTButton
-                  color="primary"
-                  block
-                  class="ml-0"
-                >
-                  Configure
-                </CTButton>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-row>
-      </template>
+                Accept all
+              </CTButton>
+              <CTButton
+                color="primary"
+                block
+                contained
+                class="mb-2 ml-0 elevation-0"
+              >
+                Reject all
+              </CTButton>
+              <CTButton
+                color="primary"
+                block
+                class="ml-0"
+              >
+                Configure
+              </CTButton>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-row>
       <!-- /Cookies -->
 
       <Nuxt />
@@ -214,21 +183,6 @@
   </v-app>
 </template>
 
-<style scoped>
-  .cookies {
-    height: 300px;
-    width: 300px;
-    position: fixed;
-    bottom: 16px;
-    right: 16px;
-    padding: 8px;
-    z-index: 1;
-  }
-  .word-break-word {
-    word-break: break-word;
-  }
-</style>
-
 <script>
 export default {
   data: () => ({
@@ -245,3 +199,18 @@ export default {
   })
 }
 </script>
+
+<style scoped>
+  .cookies {
+    height: 300px;
+    width: 300px;
+    position: fixed;
+    bottom: 16px;
+    right: 16px;
+    padding: 8px;
+    z-index: 1;
+  }
+  .word-break-word {
+    word-break: break-word;
+  }
+</style>
